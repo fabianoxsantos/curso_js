0278d2be6c0cefb59d737e1359c0923c68945e35
fabianoxsanto <fabianoxsantos@gmail.com>
2020-01-16 19:30:31 -0300

    atual

---
+++ objetos/oo.js
@@ -0,0 +1,28 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Paradigmas de programação
+
+// Procedural
+
+processamento(valor1, valor2, valor3)
+
+// OO (Orientação a objetos)
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // .....
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
+// Principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
+
+
