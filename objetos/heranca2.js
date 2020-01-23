3c54faf6753e6e16cb228a098c03d20460d6718a
fabianoxsanto <fabianoxsantos@gmail.com>
2020-01-23 11:30:01 -0300

    m

---
+++ objetos/heranca2.js
@@ -0,0 +1,48 @@
+// Cadeia de Protótipos (prototype chain)
+
+Object.prototype.attr0 = 'Z' // não faça isso em casa
+
+const avo = { attr1: 'A' }
+const pai = { __proto__: avo, attr2: 'B', attr3: '3' }
+const filho = { __proto__: pai, attr3: 'C'}
+
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelaraMais(delta) {
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        } else {
+            this.velAtual = this.velMax
+        }
+    },
+    status (){
+        return `${this.velAtual}Km/h de  ${this.velMax}Km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324  // shadowing - subscreve o valor do atributo padrao no obj carro
+}
+
+const volvo = {
+    modelo: 'V40',
+    status() {
+        return `${this.modelo} ${super.status()}`   // super. pra pegar o médoto lá do prototipo
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro) 
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelaraMais(100)
+console.log(volvo.status())
+
+ferrari.acelaraMais(300)
+console.log(ferrari.status())
---
+++ objetos/heranca4.js
@@ -0,0 +1,6 @@
+function MeuObjeto() {}
+console.log(MeuObjeto.prototype)
+
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+
