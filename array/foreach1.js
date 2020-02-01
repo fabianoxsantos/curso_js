7edaebdecf57e8239a01993360211ba0ea572f84
fabianoxsanto <fabianoxsantos@gmail.com>
2020-01-31 23:47:44 -0300

    atualiza

---
+++ array/foreach1.js
@@ -0,0 +1,13 @@
+
+const aprovados = ['Agatha', 'Ana', 'Danilo', 'Gabriel']
+
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1}) ${nome}`)
+})
+
+
+aprovados.forEach(nome => console.log(nome))
+
+
+const exibirAprovados = nome => console.log(nome)
+aprovados.forEach(exibirAprovados) 
---
+++ array/map1.js
@@ -0,0 +1,7 @@
+const nums = [1, 2, 3, 4, 5]
+
+const result = nums.map(function(e){
+    return e * 2
+})
+
+console.log(result)
