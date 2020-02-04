5085929fcaebc8aa904c4472a46eaae0c4eaefbb
fabianoxsanto <fabianoxsantos@gmail.com>
2020-02-04 19:03:55 -0300

    atualizando

---
+++ array/map2.js
@@ -0,0 +1,15 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lápis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }',
+]
+
+// Retornar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)  // transforma JSON em array de objetos
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+
+console.log (resultado)
