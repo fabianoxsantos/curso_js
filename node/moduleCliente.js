ebeb80a799b647ffdc4d70f3e319b87f0564abe2
fabianoxsanto <fabianoxsantos@gmail.com>
2020-02-13 19:30:17 -0300

    m

---
+++ node/moduleCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA')
+const moduleB = require('./moduleB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.atelog)
+console.log(moduloA)
+
+console.log(moduleB.bomdia)
+console.log(moduleB.boaNoite())
+console.log(moduleB)
---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Olá Mundo'
+exports.bemVindo = 'Bem Vindo'
+module.exports.atelog = 'Até logo'
